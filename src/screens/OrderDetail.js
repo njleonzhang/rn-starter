c17d664a1377e3f0e4f6b76370ae8e9dccec6646
njleonzhang <scream3616@sina.com>
2018-07-06 17:07:10 +0800

    update

--- src/screens/OrderDetail.js
+++ src/screens/OrderDetail.js
@@ -4,6 +4,10 @@ import { Text, View } from 'react-native'
 
 @ScreenHoc
 export default class Detail extends Component {
+  static navigationOptions = {
+    title: '订单详情'
+  }
+
   render() {
     return (
       <View>
